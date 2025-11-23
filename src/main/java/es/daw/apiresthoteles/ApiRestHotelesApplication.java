58f5dd1584cb254a54a1294805612545ec669e3b
Paula Suarez <paulaelqh@gmail.com>
2025-11-23 18:31:51 +0100

    first commit

---
+++ src/main/java/es/daw/apiresthoteles/ApiRestHotelesApplication.java
@@ -0,0 +1,13 @@
+package es.daw.apiresthoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestHotelesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestHotelesApplication.class, args);
+    }
+
+}
